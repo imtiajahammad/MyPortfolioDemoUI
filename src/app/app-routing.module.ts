022c55095129bc1fc5800ccbb04082be8e77bf54
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2020-08-30 15:18:00 +0600

    added routes to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientPanelComponent } from './client-panel/client-panel.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'client-panel',component:ClientPanelComponent},
+  {path:'admin-panel',component:AdminPanelComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[ClientPanelComponent,AdminPanelComponent]
