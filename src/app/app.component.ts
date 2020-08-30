07e222a41a5361e3c36bf523d52be1ad76021b3c
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2020-08-30 15:19:24 +0600

    added goToPage method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+/*to make buttons work*/
+import {Router} from '@angular/router';
+/*to make buttons work*/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyPortfolioDemoUI';
+
+/*to make buttons work*/
+  constructor(private router:Router){}
+
+  goToPage(pageName:string):void{
+    this.router.navigate([`${pageName}`]);
+  }
+
+  /*to make buttons work*/
 }
+
