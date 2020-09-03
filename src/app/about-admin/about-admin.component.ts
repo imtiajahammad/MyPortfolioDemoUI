22901220db372659f729be8a58757d37f890cbb2
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2020-09-04 02:47:46 +0600

    about admin component added

---
+++ src/app/about-admin/about-admin.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AboutService } from '../about.service';
+
+@Component({
+  selector: 'app-about-admin',
+  templateUrl: './about-admin.component.html',
+  styleUrls: ['./about-admin.component.css']
+})
+export class AboutAdminComponent implements OnInit {
+
+  constructor(private _aboutService:AboutService) { }
+
+  public aboutGetData=[];
+  topics=['English','Bangla','Vue'];
+
+  ngOnInit(): void {
+    this._aboutService.getAboutInfo(/*1*/)
+              .subscribe(data=>this.aboutGetData=data);
+  }
+
+}
